892796576d085d614a3a4a1cae7dcfb4dd33e8dd
FelipeGuerraInfante <felipe.guerra2101@alumnos.ubiobio.cl>
2025-06-09 14:58:43 -0400

    attemp pr

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,9 @@ import axios from 'axios';
 import { Toaster } from 'react-hot-toast';
 import { UserContextProvider } from '../context/userContext';
 
-axios.defaults.baseURL = 'http://localhost:8000';
+const API_URL = import.meta.env.VITE_BASE_URL || 'http://localhost:8000';
+
+axios.defaults.baseURL = API_URL;
 axios.defaults.withCredentials = true;
 
 function App() {
